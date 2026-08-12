c0c6df8793ad17fbc99badcfb4fdc37eb9831d91
Bober <nikolaygorda@gmail.com>
2026-08-12 11:26:01 +0200

    fix: preserve phone display orientation

--- BobrCam/Platforms/Android/MainActivity.cs
+++ BobrCam/Platforms/Android/MainActivity.cs
@@ -9,7 +9,7 @@ namespace BobrCam;
     Theme = "@style/Maui.SplashTheme",
     MainLauncher = true,
     LaunchMode = LaunchMode.SingleTop,
-    ScreenOrientation = ScreenOrientation.Portrait,
+    ScreenOrientation = ScreenOrientation.Locked,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation |
         ConfigChanges.UiMode | ConfigChanges.ScreenLayout |
         ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
